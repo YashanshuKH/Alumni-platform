396bc48672759d9de7c081ceff3a1e762851018f
Shubh-Jarvis <shubhkhetrapal1@gmail.com>
2025-09-18 15:19:52 +0530

    some changes

---
+++ src/components/example/Random.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import styles from './Random.module.css'
+const Random = () => {
+  return (
+    <div className={styles.Random}>Random</div>
+  )
+}
+
+export default Random
