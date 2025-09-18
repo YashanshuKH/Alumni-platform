51ade5caf84094899b1af49faedec7d62201abcb
YashanshuKH <yashanshukh337@gmail.com>
2025-09-18 21:13:27 +0530

    Updates

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Login from './components/Login/login'
 import Dashboard from './components/Dashboard/Dashboard';
 import Event from './components/Event/Event';
 import ResetPassword from './components/Reset/Reset'
+import ForgotPassword from './components/Forgot/forgot';
 function App() {
 
   return (
@@ -12,7 +13,9 @@ function App() {
       <Route path="/" element={<Landing />} />
       <Route path="/login" element={<Login />} />
       {/* <Route path="/signup" element={<Signup />} /> */}
-      <Route path="/reset" element={<Login />} />
+      <Route path="/forgot" element={<ForgotPassword />} />
+      
+      <Route path="/reset-password" element={<ResetPassword />} />
       <Route path="/login" element={<Login />} />
 
       <Route path="/dashboard" element={<Dashboard />} />
--- src/components/Login/login.jsx
+++ src/components/Login/login.jsx
@@ -6,50 +6,58 @@ import { Link } from "react-router-dom";
 import Navbar from "../Navbar/Navbar";
 
 const Login = () => {
-
   return (
     <div className={styles.body}>
       <Navbar />
       <h2 className={styles.title}>Log In</h2>
       <form onSubmit={(e) => e.preventDefault()} className={styles.form}>
-          <div className={styles.formGroup}>
-            <label htmlFor="firstname">
-              <IoPersonSharp /> Name :
-            </label>
-            <input type="text" name="firstname" placeholder="Name" required />
-          </div>
-          <div className={styles.formGroup}>
-            <label htmlFor="mobileno">
-              <CiMobile1 /> Mobile :
-            </label>
-            <input
-              type="text"
-              name="mobileno"
-              placeholder="Mobile Number"
-              maxLength={10}
-              inputMode="numeric"
-              pattern="\d{10}"
-              required
-            />
-          </div>
-          <div className={styles.formGroup}>
-            <label htmlFor="password">
-              <CgPassword /> Password :
-            </label>
-            <input
-              type="password"
-              name="password"
-              placeholder="Password"
-              required
-            />
-          </div>
-        
+        <div className={styles.formGroup}>
+          <label htmlFor="firstname">
+            <IoPersonSharp /> Name :
+          </label>
+          <input type="text" name="firstname" placeholder="Name" required />
+        </div>
+
+        <div className={styles.formGroup}>
+          <label htmlFor="mobileno">
+            <CiMobile1 /> Mobile :
+          </label>
+          <input
+            type="text"
+            name="mobileno"
+            placeholder="Mobile Number"
+            maxLength={10}
+            inputMode="numeric"
+            pattern="\d{10}"
+            required
+          />
+        </div>
+
+        <div className={styles.formGroup}>
+          <label htmlFor="password">
+            <CgPassword /> Password :
+          </label>
+          <input type="password" name="password" placeholder="Password" required />
+        </div>
+
+        {/* 🔹 Forgot Password Link */}
+        <div className={styles.forgotWrapper}>
+          <Link to="/forgot" className={styles.forgot}>
+            Forgot Password?
+          </Link>
+        </div>
+
         <button type="submit" className={styles.submitBtn}>
           Log in
         </button>
-        <center className={styles.not}>Don't Have an Account ? <Link className={styles.signup} to="/signup">Sign Up</Link></center>
+
+        <center className={styles.not}>
+          Don't Have an Account ?{" "}
+          <Link className={styles.signup} to="/signup">
+            Sign Up
+          </Link>
+        </center>
       </form>
-      
     </div>
   );
 };
