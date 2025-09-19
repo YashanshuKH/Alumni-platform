bfccb29b3a07d22a3a768c0e9c37f4725ab071e5
Shubh-Jarvis <shubhkhetrapal1@gmail.com>
2025-09-19 22:15:45 +0530

    some changes

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Landing from './components/Landing/Landing';
 import Login from './components/Login/login'
 import Dashboard from './components/Dashboard/Dashboard';
 import Event from './components/Event/Event';
+import Admin from './components/Admin/Admin';
 
 function App() {
 
@@ -13,6 +14,7 @@ function App() {
       <Route path="/login" element={<Login />} />
       <Route path="/dashboard" element={<Dashboard />} />
       <Route path="/random" element={<Event />} />
+      <Route path="/admin" element={<Admin />} />
     </Routes>
 
   )
